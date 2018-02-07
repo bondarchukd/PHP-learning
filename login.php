b4b1b6a3151ed65e3aaead5383b15fe35aa98b5f
bondarchukd <31472412+bondarchukd@users.noreply.github.com>
2018-02-07 17:49:45 +0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,21 @@
+<?php
+
+$email = $_POST["email"];
+$password = $_POST["password"];
+
+$connection = mysqli_connect("localhost","","","test");
+$query = mysqli_query($connection,"select * from 'users' where PASSWORD = '"$password"' and EMAIL = '"$email"';");
+$result = mysqli_query($connection, $query) or die (mysqli_error($connection));
+$count = mysqli_num_rows($result);
+if ($count == 1) {
+	$_SESSION["email"] = $email;
+} else {
+	$fmsg = "Invalid lolin or password";
+}
+
+if (isset ($_SESSION['email'])) {
+	$email = $_SESSION['email'];
+echo "Hai" . $email . "";
+echo "<a href='logout.php'>Logout</a>";	
+} else{}
+?>
