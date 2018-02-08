e676ee45b4bcf4c88c3fa809d45f1040519f8e1d
Michael <mishas96@me.com>
2018-02-08 19:19:19 +0300

    add pass

--- register.php
+++ register.php
@@ -3,7 +3,7 @@
 $email = $_POST["email"];
 $password = $_POST["password"];
 
-$connection = mysqli_connect("localhost","","","test");
+$connection = mysqli_connect("sql131.main-hosting.eu","u386269602_root","dbondarchuk1","u386269602_test");
 $result = mysqli_query($connection,"insert into users (PASSWORD, EMAIL) values ('$password', '$email')");
 
 echo "success";
