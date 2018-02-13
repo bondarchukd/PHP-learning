574149b408b0914323dcac1f5b3907c58796cf85
bondarchukd <dbondarchuk@gmail.com>
2018-02-13 17:24:50 +0300

    user.php

--- user.php
+++ user.php
@@ -12,7 +12,7 @@ $connection = mysqli_connect('localhost', '', '', 'test');
 $result = mysqli_query($connection,"SELECT * from Users WHERE Email='".$_SESSION['login']."' and Password='".$_SESSION['pass']."'");
 
 
-if(mysql_num_rows($result)!=1){    //такого пользователя нет
+if(mysqli_num_rows($result)!=1){    //такого пользователя нет
 
     Header("Location: http://localhost:8888/PHP-learning/login.html");  //перенаправляем на login.php   
 }
