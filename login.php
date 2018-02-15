eb549fa9ffb9df8ed89cb111ff545cdc251eaf3d
bondarchukd <dbondarchuk@gmail.com>
2018-02-15 18:37:52 +0300

    1
    1

--- login.php
+++ login.php
@@ -5,7 +5,7 @@ $password = $_POST["password"];
 $username = $_POST["username"];
 
 $connection = mysqli_connect("mysql.j820528.myjino.ru", "j820528", "dbondarchuk1",  "j820528_test");
-$result = mysqli_query($connection,"select Username, Password, Email from Users where values ('$username','$password', '$email')");
+$result = mysqli_query($connection,"select Username, Password, Email from Users where Username ='$username' and Password = '$password' and Email = '$email')");
 
 if(mysqli_num_rows($result)!=1){    //такого пользователя нет
 
