ff76bae99f2575a7eaf4114af783711b30fa4352
bondarchukd <dbondarchuk@gmail.com>
2018-02-16 17:56:38 +0300

    1
    1

--- user.php
+++ user.php
@@ -7,26 +7,25 @@ date_default_timezone_set('Europe/Berlin');
 $Hour = date('G');
 
 if ( $Hour >= 5 && $Hour <= 11 ) {
-    $Greeting = "Good Morning";
+    $Greeting = "Good Morning!";
 } else if ($Hour >= 12 && $Hour <= 18 ) {
-    $Greeting = "Good Afternoon";
+    $Greeting = "Good Afternoon!";
 } else if ( $Hour >= 19 || $Hour <= 4 ) {
-    $Greeting = "Good Evening";
+    $Greeting = "Good Evening!";
 }
 echo "<h1>".$Greeting."</h1>";
-echo "<h1>You have been registered successfuly!</h1>";
+
+
+echo "<h3>You have been registered successfuly</h3>";
 
 session_start();
-// echo $_SESSION['login'];
-// echo $_SESSION['pass'];
-// $connection = mysqli_connect("sql131.main-hosting.eu","u386269602_root","dbondarchuk1","u386269602_test");
-// $connection = mysqli_connect('localhost', '', '', 'test');
+
 $connection = mysqli_connect("mysql.j820528.myjino.ru", "j820528", "dbondarchuk1", "j820528_test");
 
 
 // connection checking
 if (isset($connection)) {
-	echo 'Connected <br>';
+	echo ' Status: connected <br>';
 }
 
 $result = mysqli_query($connection,"SELECT * from Users WHERE Email='".$_SESSION['login']."' and Password='".$_SESSION['pass']."'");
@@ -39,9 +38,9 @@ if(mysqli_num_rows($result)!=1){    //такого пользователя не
 }
 else{   //пользователь найден, можем выводить все что нам надо
     echo "Welcome " . $_SESSION['login'] . "! <br>";
-    echo "<a href ='logout.php'>Logout</a><br>";
     echo "<a href ='database_page.php'><button>Admin page</button></a>";
-    echo "<a href = 'weather.html'><button>Get weather!</button></a>";
+    echo "<a href = 'weather.html'><button>Get weather!</button></a><br><br>";
+    echo "<a href ='logout.php'>Logout</a><br>";
 }
 
 
