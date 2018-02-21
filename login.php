643782c353e97bb7233b1787014a082fa8855f1a
bondarchukd <dbondarchuk@gmail.com>
2018-02-21 06:56:10 +0300

    nth
    nth

--- login.php
+++ login.php
@@ -6,10 +6,10 @@
 session_start();
 $email = $_POST["email"];
 $password = $_POST["password"];
-$username = $_POST["username"];
+// $username = $_POST["username"];
 
 $connection = mysqli_connect("mysql.j820528.myjino.ru", "j820528", "dbondarchuk1",  "j820528_test");
-$result = mysqli_query($connection,"select Username, Password, Email from Users where Username ='$username' and Password = '$password' and Email = '$email')");
+$result = mysqli_query($connection,"select Password, Email from Users where Password = '$password' and Email = '$email')");
 
 if(mysqli_num_rows($result)!=1){    //такого пользователя нет
 
@@ -18,7 +18,7 @@ if(mysqli_num_rows($result)!=1){    //такого пользователя не
     Header("login.html");
 }
 else{
-$_SESSION["user_id"] = $username;
+// $_SESSION["user_id"] = $username; // should it be in sessoin?
 $_SESSION['login']=$email;
 $_SESSION['pass']=$password;
 
