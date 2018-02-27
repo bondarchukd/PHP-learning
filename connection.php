39f5192c3e0bf8094fb797ad0d81550f0fb438eb
bondarchukd <dbondarchuk@gmail.com>
2018-02-27 16:45:03 +0300

    Queries move to enter.php
    Enter.php does not work

--- connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// $connection = mysqli_connect("mysql.j820528.myjino.ru", "j820528", "dbondarchuk1", "j820528_test");
-
-// if (isset($connection)) {
-// 	echo ' Status: connected <br>';
-// }
-
-?>
--- index.php
+++ index.php
@@ -1,49 +1,28 @@
 <?php
-
 require_once('database.php');
 session_start();
-
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home page</title>
+</head>
+<body>
+<?php 
 if($_GET) {
 	if ($_GET["status"] == 1) {
 		echo "Registed";
 		echo "<br><br><h1>Welcome</h1><br>";
-		if (isset($_SESSION["username")) {
+		if (isset($_SESSION["username"])) {
 		echo $_SESSION["username"];
+	}
 		}
-	}else{
-		echo "Logined";
+	else {		
+			echo "Logined";
+			echo "<br><br><h1>Welcome</h1><br>";
 	}
-}
-
-// Using $_GET for entering to another page
-?>
-<html>
-<a href = 'database_page.php?hello=true'> Go to database_page through GET</a>
-</html>
-<?php
-function dbPage() {
-	if(isset($_GET['hello'])) {
-		Header("Location: http://localhost/PHP-learning/database_page.php");
-	}
-}
-?>
-<!-- ///////////////////////////////////////////////// -->
-
-
-<?php
-$result = mysqli_query($connection,"SELECT * from Users WHERE Email='".$_SESSION['login']."' and Password='".$_SESSION['pass']."'");
-
-if(mysqli_num_rows($result)!=1){    //такого пользователя нет
-
-    Header("Location: http://localhost:8888/PHP-learning/login.html");//перенаправляем на login.php
-    // Header("login.html");
-    // Header("Location: http://localhost/PHP-learning/login.html"); //перенаправляем на login.php
-}
-else {
-
+}	
 date_default_timezone_set('Europe/Moscow');
-
-
 // SET TIME-ZONE GREETING
 // G - 24-hour format of an hour without leading zeros (0 through 23)
 $Hour = date('G');
@@ -55,15 +34,10 @@ if ( $Hour >= 5 && $Hour <= 11 ) {
 } else if ( $Hour >= 19 || $Hour <= 4 ) {
     $Greeting = "Good Evening!";
 }
-
-
 echo "<h1>".$Greeting."</h1>";
-echo "<h3>You have been registered successfuly</h3>";
-echo "Welcome " . $_SESSION['login'] . "! <br>";
 echo "<a href ='database_page.php'><button>Admin page</button></a>";
 echo "<a href = 'weather.html'><button>Get weather!</button></a><br><br>";
 echo "<a href ='logout.php'>Logout</a><br>";
-}
-
 ?>
-
+</body>
+</html>
