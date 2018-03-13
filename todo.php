62be5933a36a40b73bf281357eb092ad3f0dcd2a
bondarchukd <dbondarchuk@gmail.com>
2018-03-13 06:45:00 +0300

    1
    1

--- todo.php
+++ todo.php
@@ -14,7 +14,24 @@ if (!isset($_SESSION['email']) && !isset($_SESSION['password'])) {
 	<title>My To-Do list</title>
 	<!-- JQUERY -->
 	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-		
+	<!-- TO DO list script -->
+	<script type="text/javascript">
+	$(function() {
+	$("#add").on("click", function()
+{
+	var val = $("input").val();
+	if(val!== '') {
+		var elem = $("<li></li>").text(val);
+		$(elem).append("<button class='rem'>X</button>");
+		$("#mylist").append(elem);
+		$("input").val("");
+		$(".rem").on("click"), function() {
+			$(this).parent().remove();
+		};
+}
+});
+});
+</script>	
 	<link rel = "stylesheet" type = "text/css" href = "CSS.css">
 </head>
 <body>
@@ -24,12 +41,11 @@ if (!isset($_SESSION['email']) && !isset($_SESSION['password'])) {
 <?php
 require('dropdown.html');
 ?>
-
 <br>
 
 <!-- input for adding new item -->
 <input type="text" placeholder="New item">
 <button id = "add">Add</button>
-<ol id"mylist"></ol>
+<ol id="mylist"></ol>
 </body>
 </html>
