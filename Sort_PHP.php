17e9fa6c2f94480d78416c57246d9b29e2568055
bondarchukd <dbondarchuk@gmail.com>
2018-03-15 17:44:42 +0300

    1
    1

--- Sort_PHP.php
+++ Sort_PHP.php
@@ -2,32 +2,39 @@
 // https://stackoverflow.com/questions/27085580/sort-mysql-tables-using-php
 // https://stackoverflow.com/questions/3489783/how-to-sort-rows-of-html-table-that-are-called-from-mysql
 
-require 'session.php';
-$sort = $_GET['s'];
-
+require ('database.php');
+// $sort = $_GET['s'];
+$desc = "DESC";
 echo "<h1> List of registered users</h1>";
 
-// Example from tutorial
-$connection = mysqli_connect("mysql.j820528.myjino.ru", "j820528", "dbondarchuk1",  "j820528_test");
- 
-// Check connection
-if($connection === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-}
-
-// Sorting 
-// $sort = $_GET['s'];
-$sql="SELECT * FROM users ORDER BY $sort";
+$sql = "SELECT * FROM users ORDER BY ID " .$desc."";
 if($result = mysqli_query($connection, $sql)){
     if(mysqli_num_rows($result) > 0){
 echo '<table>';        
 echo '<tr>';
-echo '<td><a href="db_sorted_php.php?s=title">ID</a><td>';
-echo '<td><a href="db_sorted_php.php?s=album">USERNAME</a><td>';
-echo '<td><a href="db_sorted_php.php?s=artist">EMAIL</a><td>';
+echo '<td width = 50px align = center><a href="db_sorted_php.php?s=title">ID</a><td>';
+echo '<td width = 50px align = center><a href="db_sorted_php.php?s=album">USERNAME</a><td>';
+echo '<td width = 50px align = center><a href="db_sorted_php.php?s=artist">EMAIL</a><td>';
 echo '</tr>';
 echo '</table';
-}}
+
+while($row = mysqli_fetch_array($result)){
+            echo "<tr>";
+                echo "<td width = 50px align = center>" . $row['ID'] . "</td>";
+                echo "<td width = 70px align = center>" . $row['USERNAME'] . "</td>";
+                echo "<td width = 70px align = center>" . $row['EMAIL'] . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+        // Free result set
+        mysqli_free_result($result);
+    } else{
+        echo "No records matching your query were found.";
+    }
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($connection);
+}
+
 
 // Attempt select query execution
 // $sql = "SELECT * FROM users";
