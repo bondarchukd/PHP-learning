766930095fe6faead8042a994dc196deb9939e2b
bondarchukd <dbondarchuk@gmail.com>
2018-03-27 17:58:07 +0300

    1
    1

--- table_pages.php
+++ table_pages.php
@@ -3,9 +3,33 @@
 require_once('database.php');
 require_once('check_enter.php');
 
-if (isset($_GET["page"])) { $page  = $_GET["page"]; } else { $page=1; };
+error_reporting(0);
+$servername = "mysql.j820528.myjino.ru";
+$username = "j820528";
+$password = "dbondarchuk1";
+$dbname = "j820528_test";
+$datatable = "users"; // MySQL table name
+$results_per_page = 5; // number of results per page
+ 
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+
+// --------------------------------------
+
+$results_per_page = 5;
+
+if (isset($_GET["page"])) { 
+	$page  = $_GET["page"]; 
+} else { 
+	$page = 1; 
+}
 $start_from = ($page-1) * $results_per_page;
-$sql = "SELECT * FROM ".$datatable." ORDER BY ID ASC LIMIT $start_from, ".$results_per_page;
+$sql = "SELECT * FROM users ORDER BY ID ASC LIMIT $start_from, ".$results_per_page;
 $rs_result = $conn->query($sql);
 ?> 
 <table border="1" cellpadding="4">
@@ -16,9 +40,12 @@ $rs_result = $conn->query($sql);
  while($row = $rs_result->fetch_assoc()) {
 ?> 
             <tr>
-            <td><? echo $row["ID"]; ?></td>
-            <td><? echo $row["Name"]; ?></td>
-            <td><? echo $row["PhoneNumber"]; ?></td>
+            <td><? echo $row["ID"]; 
+            	?></td>
+            <td><? echo $row["Username"]; 
+            	?></td>
+            <td><? echo $row["Email"]; 
+            	?></td>
             </tr>
 <?php 
 }; 
@@ -26,7 +53,7 @@ $rs_result = $conn->query($sql);
 </table>
   
 <?php 
-$sql = "SELECT COUNT(ID) AS total FROM ".$datatable;
+$sql = "SELECT COUNT(ID) AS total FROM users";
 $result = $conn->query($sql);
 $row = $result->fetch_assoc();
 $total_pages = ceil($row["total"] / $results_per_page); // calculate total pages with results
