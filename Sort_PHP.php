284e553a7f93f23358cdce45c982987be7cb0dbe
bondarchukd <dbondarchuk@gmail.com>
2018-03-27 17:46:10 +0300

    table_pages.php
    1

--- Sort_PHP.php
+++ Sort_PHP.php
@@ -4,11 +4,11 @@
 
 require_once('database.php');
 require_once('check_enter.php');
-// $sort = $_GET['sort'];
-$desc = "DESC";
+
 echo "<h1> List of registered users</h1>";
 
-$sql = "SELECT * FROM users ORDER BY ID " .$desc."";
+
+$sql = "SELECT * FROM users ORDER BY ID ASC LIMIT 0, 5";
 if($result = mysqli_query($connection, $sql)){
     if(mysqli_num_rows($result) > 0){
 echo "<table bgcolor =#C0C0C0>";
@@ -34,37 +34,5 @@ while($row = mysqli_fetch_array($result)){
     echo "ERROR: Could not able to execute $sql. " . mysqli_error($connection);
 }
 
-
-// Attempt select query execution
-// $sql = "SELECT * FROM users";
-// $result = mysqli_query($connection, $sql);
-
-// if($result = mysqli_query($connection, $sql)){
-//     if(mysqli_num_rows($result) > 0){
-//         echo "<table bgcolor =#C0C0C0>";
-//             echo "<tr>";
-//                 echo "<th>ID</th>";
-//                 echo "<th>USERNAME</th>";
-//                 echo "<th>EMAIL</th>";
-//             echo "</tr>";
-//         while($row = mysqli_fetch_array($result)){
-//             echo "<tr>";
-//                 echo "<td width = 50px align = center>" . $row['ID'] . "</td>";
-//                 echo "<td width = 70px align = center>" . $row['USERNAME'] . "</td>";
-//                 echo "<td width = 70px align = center>" . $row['EMAIL'] . "</td>";
-//             echo "</tr>";
-//         }
-//         echo "</table>";
-//         // Free result set
-//         mysqli_free_result($result);
-//     } else{
-//         echo "No records matching your query were found.";
-//     }
-// } else{
-//     echo "ERROR: Could not able to execute $sql. " . mysqli_error($connection);
-// }
- 
-// Close connection - Should be it?
-mysqli_close($connection);
 echo "<a href='logout.php'>Logout</a>";		
 ?>
---
+++ table_pages.php
@@ -0,0 +1,40 @@
+<?php
+// https://www.phpjabbers.com/php--mysql-select-data-and-split-on-pages-php25.html
+require_once('database.php');
+require_once('check_enter.php');
+
+if (isset($_GET["page"])) { $page  = $_GET["page"]; } else { $page=1; };
+$start_from = ($page-1) * $results_per_page;
+$sql = "SELECT * FROM ".$datatable." ORDER BY ID ASC LIMIT $start_from, ".$results_per_page;
+$rs_result = $conn->query($sql);
+?> 
+<table border="1" cellpadding="4">
+<tr>
+    <td bgcolor="#CCCCCC"><strong>ID</strong></td>
+    <td bgcolor="#CCCCCC"><strong>Name</strong></td><td bgcolor="#CCCCCC"><strong>Phone</strong></td></tr>
+<?php 
+ while($row = $rs_result->fetch_assoc()) {
+?> 
+            <tr>
+            <td><? echo $row["ID"]; ?></td>
+            <td><? echo $row["Name"]; ?></td>
+            <td><? echo $row["PhoneNumber"]; ?></td>
+            </tr>
+<?php 
+}; 
+?> 
+</table>
+  
+<?php 
+$sql = "SELECT COUNT(ID) AS total FROM ".$datatable;
+$result = $conn->query($sql);
+$row = $result->fetch_assoc();
+$total_pages = ceil($row["total"] / $results_per_page); // calculate total pages with results
+  
+for ($i=1; $i<=$total_pages; $i++) {  // print links for all pages
+            echo "<a href='index.php?page=".$i."'";
+            if ($i==$page)  echo " class='curPage'";
+            echo ">".$i."</a> "; 
+}; 
+?>
+
