273dc1a94f89e44747de138a83de25aad662ebc8
bondarchukd <dbondarchuk@gmail.com>
2018-03-28 06:32:06 +0300

    sort_php rename to table.php

--- table.php
+++ table.php
@@ -1,6 +1,10 @@
 <?php
+// https://stackoverflow.com/questions/27085580/sort-mysql-tables-using-php
+// https://stackoverflow.com/questions/3489783/how-to-sort-rows-of-html-table-that-are-called-from-mysql
+// https://www.phpjabbers.com/php--mysql-select-data-and-split-on-pages-php25.html
+
 require_once('database.php');
-session_start();
+require_once('check_enter.php');
 ?>
 
 <!DOCTYPE html>
@@ -13,36 +17,38 @@ session_start();
 <body>
 
 <?php
+
 if (!isset($_SESSION['email']) && !isset($_SESSION['password'])) {
 
-    header('Location: enter.php');
+    header('Location: login.php');
 }
 
-echo "<h1> List of registered users</h1>";
+echo "<h1> List of users</h1>";
 
 // DROP DOWN menu
 require('dropdown.html');
-?>
-
-<br>
-
-<?php
+echo "<br>";
 echo "<a href = 'index.php?status=2'><button>Previous page</button></a><br><br>";
-echo "You are logined as " .$_SESSION['username']."";
 
-// // Attempt select query execution
-$sql = "SELECT * FROM users ORDER BY ID";
-$result = mysqli_query($connection, $sql);
+// PAGES
+if (isset($_GET["page"])) { 
+    $page  = $_GET["page"]; 
+} else { 
+    $page = 1; 
+}
+$results_per_page = 5;
+$start_from = ($page-1) * $results_per_page;
 
+$sql = "SELECT * FROM users ORDER BY ID ASC LIMIT $start_from, ".$results_per_page;
 if($result = mysqli_query($connection, $sql)){
     if(mysqli_num_rows($result) > 0){
-        echo "<table bgcolor =#C0C0C0>";
+echo "<table bgcolor =#C0C0C0>";
             echo "<tr>";
-                echo "<th>ID</th>";
-                echo "<th>USERNAME</th>";
-                echo "<th>EMAIL</th>";
+                echo "<th><a href='table.php?sort=id'>ID</th>";
+                echo "<th><a href='table.php?sort=username'>USERNAME</th>";
+                echo "<th><a href='table.php?sort=email'>EMAIL</th>";
             echo "</tr>";
-        while($row = mysqli_fetch_array($result)){
+while($row = mysqli_fetch_array($result)){
             echo "<tr>";
                 echo "<td width = 50px align = center>" . $row['ID'] . "</td>";
                 echo "<td width = 70px align = center>" . $row['USERNAME'] . "</td>";
@@ -58,12 +64,22 @@ if($result = mysqli_query($connection, $sql)){
 } else{
     echo "ERROR: Could not able to execute $sql. " . mysqli_error($connection);
 }
- 
-// // Close connection - Should be it?
-// mysqli_close($connection);
 
-echo "<br>";
-echo "<a href='logout.php'>Logout</a>";		
+// separate to pages
+$sql = "SELECT COUNT(ID) AS total FROM users";
+$result = mysqli_query($connection, $sql);
+$row = mysqli_fetch_array($result);
+$total_pages = ceil($row["total"] / $results_per_page); // calculate total pages with results
+
+for ($i=1; $i<=$total_pages; $i++) {  // print links for all pages
+            echo "<a href='table.php?page=".$i."'";
+            if ($i==$page)  echo " class='curPage'";
+            echo ">".$i."</a> "; 
+}; 
+
+echo "<br><br>";		
+echo "<a href='logout.php'>Logout</a>";
+echo "<br><br>";     
 ?>
 
 </body>
