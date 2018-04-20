f0f9a0d527128f40670ffe86eeca91dd70f1f620
bondarchukd <dbondarchuk@gmail.com>
2018-04-20 17:46:18 +0300

    1

--- search.php
+++ search.php
@@ -23,67 +23,36 @@
      
         if(mysqli_num_rows($raw_results) > 0){ // if one or more rows are returned do following
              
-<<<<<<< HEAD
-<<<<<<< HEAD
+
             echo "<a href = 'table.php'><button>Back</button></a><br><br>";
             echo "<p><h2>Result</h2></p>";
             
+            echo "<table>";
+                echo "<tr>";
+                    echo "<th>EMAIL</th>";
+                    echo "<th>USERNAME</th>";
+                echo "</tr";
 
             while($result = mysqli_fetch_array($raw_results)){
-            // $results = mysql_fetch_array($raw_results) puts data from database into array, while it's valid it does the loop
-            echo "<table>";
+               
                 echo "<tr>";
                     echo "<td width = 50px align = center>".$result['EMAIL']."</td>";
                     echo "<td width = 50px align = center>".$result['USERNAME']."</td>";
-                echo "</tr";
+                echo "</tr>";
             echo "</table>";        
-            } 
-            //     echo "<p><h3>".$result['EMAIL']."</h3>".$result['USERNAME']."</p>";
-=======
-=======
->>>>>>> 618335a012bc1a25592193304aaded5898a067b0
-            echo "<table>";
-                    echo "<tr>";
-                        echo "<th>EMAIL</th>";
-                        echo "<th>USERNAME</th>";
-                    echo "</tr";
 
-            while($result = mysqli_fetch_array($raw_results)){
-            // $results = mysql_fetch_array($raw_results) puts data from database into array, while it's valid it does the loop
-               
-                    echo "<tr>";
-                        echo "<td width = 50px align = center>".$result['EMAIL']."</td>";
-                        echo "<td width = 50px align = center>".$result['USERNAME']."</td>";
-                    echo "</tr>";
-                echo "</table>";        
-             
-                // echo "<p><h3>".$result['EMAIL']."</h3>".$result['USERNAME']."</p>";
-<<<<<<< HEAD
->>>>>>> 618335a012bc1a25592193304aaded5898a067b0
-=======
->>>>>>> 618335a012bc1a25592193304aaded5898a067b0
-
-            // }
-             
         }
+    }
         else{ // if there is no matching rows do following
             echo "No results";
         }
          
     }
+
     else{ // if query length is less than minimum
         echo "Minimum length is ".$min_length;
     }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-    
-=======
-    echo "<br><br><a href = 'table.php'><button>Back</button></a><br><br>";
->>>>>>> 618335a012bc1a25592193304aaded5898a067b0
-=======
-    echo "<br><br><a href = 'table.php'><button>Back</button></a><br><br>";
->>>>>>> 618335a012bc1a25592193304aaded5898a067b0
 ?>
 </body>
 </html>
